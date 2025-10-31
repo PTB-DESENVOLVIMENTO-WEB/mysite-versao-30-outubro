c306228e895fddd8c4f29bd2781a950308f02507
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-10-31 00:05:42 -0300

    Update email.py

--- app/email.py
+++ app/email.py
@@ -1,30 +1,28 @@
 from flask import current_app
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+
+from sendgrid.helpers.mail import Mail, Email
 
 def send_email_sendgrid(to_list, subject, html_content_body):
-    """Envia e-mail com SendGrid."""
+    """Envia e-mail com SendGrid de forma robusta."""
     
-    # Carrega as configs do app atual
     config = current_app.config
     full_subject = f"{config['FLASKY_MAIL_SUBJECT_PREFIX']} {subject}"
 
-    # Prepara o formato do destinatário (string ou tuple)
-    if len(to_list) == 1:
-        destinos = to_list[0]
-    else:
-        destinos = tuple(to_list)
+    from_email = Email(config['API_FROM'])
+
+    to_emails = [Email(email) for email in to_list]
 
     message = Mail(
-        from_email=config['API_FROM'],
-        to_emails=destinos,
+        from_email=from_email,
+        to_emails=to_emails,
         subject=full_subject,
         html_content=html_content_body
     )
+
     try:
         sg = SendGridAPIClient(config['SENDGRID_API_KEY'])
         response = sg.send(message)
         print(f"E-mail enviado para {to_list}, status: {response.status_code}")
     except Exception as e:
-        # Pega o contexto do app para imprimir o erro no log
         print(f"Erro ao enviar e-mail: {e}")
