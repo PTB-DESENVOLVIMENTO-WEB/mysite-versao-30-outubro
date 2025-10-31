9c2b19f485d859df99e38ba3031ce2eaa4785273
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-10-30 23:37:57 -0300

    Update flasky.py

--- flasky.py
+++ flasky.py
@@ -1,25 +1,18 @@
 import os
-import click
-from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do .env
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
 from app import create_app, db
 from app.models import User, Role
+from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
-
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
-
-
-@app.cli.command()
-@click.argument('test_names', nargs=-1)
-def test(test_names):
-    """Run the unit tests."""
-    import unittest
-    if test_names:
-        tests = unittest.TestLoader().loadTestsFromNames(test_names)
-    else:
-        tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
