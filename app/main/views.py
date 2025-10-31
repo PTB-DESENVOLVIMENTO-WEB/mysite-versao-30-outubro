4133a0167d7caf5568ec572906c11996de441ab0
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-10-30 23:39:32 -0300

    Implement email notification for new user signup
    Added email notification logic for new user registration.

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,9 @@
 from flask import render_template, session, redirect, url_for, current_app
-from .. import db
-from ..models import User
-from ..email import send_email, send_simple_message
 from . import main
 from .forms import NameForm
-
+from .. import db
+from ..models import User, Role
+from ..email import send_email_sendgrid # <-- IMPORTAMOS NOSSA FUNÇÃO
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,20 +11,49 @@ def index():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+            # Cria o usuário
+            user_role = Role.query.filter_by(name='User').first()
+            if User.query.count() == 0: # Bônus: primeiro usuário é admin
+                 user_role = Role.query.filter_by(name='Administrator').first()
+            
+            user = User(username=form.name.data, role=user_role)
             db.session.add(user)
             db.session.commit()
+            
+            # --- LÓGICA DE E-MAIL ADICIONADA ---
+            config = current_app.config # Pega as configs (API_FROM, STUDENT_ID, etc)
+            
+            # Lista de destinatários (incondicional)
+            destinatarios = [
+                config['FLASKY_ADMIN'], # Seu e-mail institucional
+                'flaskaulasweb@zohomail.com' # E-mail do professor
+            ]
+            new_user_name = form.name.data
+            
+            # Corpo do e-mail com dados do aluno
+            html_body = f"""
+                <h3>Novo Usuário Cadastrado!</h3>
+                <p><strong>Prontuário do Aluno:</strong> {config['STUDENT_ID']}</p>
+                <p><strong>Nome do Aluno:</strong> {config['STUDENT_NAME']}</p>
+                <hr>
+                <p><strong>Nome do novo usuário:</strong> {new_user_name}</p>
+            """
+            
+            # Chama a função de envio
+            send_email_sendgrid(
+                to_list=destinatarios,
+                subject='Novo usuário',
+                html_content_body=html_body
+            )
+            # --- FIM DA LÓGICA DE E-MAIL ---
+            
             session['known'] = False
-            print('FLASKY_ADMIN: ' + str(current_app.config['FLASKY_ADMIN']), flush=True)
-            if current_app.config['FLASKY_ADMIN']:
-                #send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
-                print('Enviando mensagem...', flush=True)
-                send_simple_message([current_app.config['FLASKY_ADMIN'], "flaskaulasweb@zohomail.com"], 'Novo usuário', form.name.data)
-                print('Mensagem enviada...', flush=True)
         else:
             session['known'] = True
+            
         session['name'] = form.name.data
         return redirect(url_for('.index'))
+        
     return render_template('index.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
