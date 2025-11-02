650608d682c2818314df99cb123d39828f979563
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-11-02 19:31:40 -0300

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Optional
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('Qual é o seu nome?', validators=[DataRequired()])
+    # --- NOVO CAMPO ADICIONADO ---
+    email = StringField('Qual é o seu email (Envio de notificação de novo usuário)?', 
+                        validators=[Optional(), Email()])
     submit = SubmitField('Submit')
