fff425cd79497022396173472fe3812671242572
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-11-03 21:08:24 -0300

    Add email field to NameForm
    Added email field to NameForm for user notifications.

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email, Optional
 
 class NameForm(FlaskForm):
     name = StringField('Qual é o seu nome?', validators=[DataRequired()])
-    # --- NOVO CAMPO ADICIONADO ---
+   
     email = StringField('Qual é o seu email (Envio de notificação de novo usuário)?', 
                         validators=[Optional(), Email()])
     submit = SubmitField('Submit')
